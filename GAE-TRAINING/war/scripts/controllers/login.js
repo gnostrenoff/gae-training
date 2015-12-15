b1ff9c6c7002c89f3c70d22215b05c3b7d938445
louis <amoros.louis@gmail.com>
2015-12-15 10:49:20 +0100

    Add google sign in button. Still issue when try to sing in.

---
+++ GAE-TRAINING/war/scripts/controllers/login.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gaeTrainingApp').controller('LoginCtrl',
+		LoginCtrlFnt);
+
+LoginCtrlFnt.$inject = [ '$scope', '$log' ];
+
+function LoginCtrlFnt($scope, $log) {
+	
+}
